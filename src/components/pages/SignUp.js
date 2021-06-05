49ef6fb30bd0c97fc051ba3ec3cdae548561d0b7
Britt-hub <britloy91@gmail.com>
2021-06-05 13:30:58 -0400

    clean up

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,47 +1,18 @@
-// import "../../App.css"
-// import React, { Component } from 'react';
-// import HeroSection from "../HeroSection";
-// import Cards from "../Cards";
-// import Footer from "../Footer";
-
-// function SignUp() {
-//     return (
-//         <>
-//             <HeroSection />
-//             <Cards />
-//             <Footer />
-//         </>
-//     )
-// }
-
-// // export default function SignUp() {
-// //     return <h1 className="sign-up">Sign-up</h1>
-// // }
-
-// export default SignUp
-
 import React, { Component } from 'react';
 import SignupForm from '../signupForm';
 import Footer from "../Footer";
 
-function SignUp() {
-    return (
-        <>
-            <SignupForm />
-            <Footer />
-        </>
-        
-    )
+
+class Signup extends Component {
+    render() {
+        return (
+            <div className='sign-up'>
+                <SignupForm />
+                <Footer />
+            </div>
+        )
+    }
 }
 
-// class Signup extends Component {
-//     render() {
-//         return (
-//             <div className='sign-up'>
-//                 <SignupForm/>
-//             </div>
-//         )
-//     }
-// }
-
 export default Signup;
+
